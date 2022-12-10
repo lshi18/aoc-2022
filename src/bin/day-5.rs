3ebaa0fa9e4f0e1392940a80294e79dcdde96673
Lei Shi <lei.shi@relexsolutions.com>
2022-12-10 14:26:17 +0200

    Day5: refactor and play around with types

--- src/bin/day-5.rs
+++ src/bin/day-5.rs
@@ -1,37 +1,36 @@
 use std::fs;
 
 fn main() {
-    let part1 = |mut acc: Vec<Vec<char>>, (cnt, from, to): &(u32, usize, usize)| {
-        for _i in 0..*cnt {
+    let (stacks, instrs) = init();
+
+    let part1 = |mut acc: Vec<Vec<char>>, &(cnt, from, to): &(usize, usize, usize)| {
+        for _i in 0..cnt {
             // Not particularly nice !!!
-            let v = acc[*from - 1].pop().unwrap();
-            acc[*to - 1].push(v);
+            let v = acc[from - 1].pop().unwrap();
+            acc[to - 1].push(v);
         }
         acc
     };
 
-    let res1 = solution(part1);
-    println!("part 1 res: {:?}", res1);
+    let res1 = solution(stacks.clone(), &instrs, part1);
+    assert!(res1 == "QMBMJDFTD", "expected QMBMJDFTD, got {}", res1);
 
-    let part2 = |mut acc: Vec<Vec<char>>, (cnt, from, to): &(u32, usize, usize)| {
+    let part2 = |mut acc: Vec<Vec<char>>, &(cnt, from, to): &(usize, usize, usize)| {
         let mut tmp: Vec<char> = vec![];
-        for _i in 0..*cnt {
+        for _i in 0..cnt {
             // Not particularly nice !!!
-            let v = acc[*from - 1].pop().unwrap();
+            let v = acc[from - 1].pop().unwrap();
             tmp.insert(0, v);
         }
-        acc[*to - 1].append(&mut tmp);
+        acc[to - 1].append(&mut tmp);
         acc
     };
 
-    let res2 = solution(part2);
-    println!("part 2 res: {:?}", res2);
+    let res2 = solution(stacks, &instrs, part2);
+    assert!(res2 == "NBTVTJNFJ", "expected NBTVTJNFJ, got {}", res2)
 }
 
-fn solution<F>(f: F) -> String
-where
-    F: FnMut(Vec<Vec<char>>, &(u32, usize, usize)) -> Vec<Vec<char>>,
-{
+fn init() -> (Vec<Vec<char>>, Vec<(usize, usize, usize)>) {
     let input = String::from_utf8(fs::read("input-day5.txt").unwrap()).unwrap();
 
     let res = input.trim_end().split("\n\n").collect::<Vec<_>>();
@@ -48,7 +47,7 @@ where
         vec![],
         vec![],
     ];
-    let moves: Vec<(u32, usize, usize)> = vec![];
+    let moves: Vec<(usize, usize, usize)> = vec![];
 
     let stacks = res[0].split('\n').rfold(stacks, |mut acc, x| {
         let x = x.chars().collect::<Vec<_>>();
@@ -65,19 +64,26 @@ where
     let instrs = res[1].split('\n').fold(moves, |mut acc, s| {
         let s = s.split_ascii_whitespace().collect::<Vec<_>>();
         acc.push((
-            s[1].parse::<u32>().unwrap(),
+            s[1].parse::<usize>().unwrap(),
             s[3].parse::<usize>().unwrap(),
             s[5].parse::<usize>().unwrap(),
         ));
         acc
     });
 
-    //println!("2: {:?}", instrs);
-    let stacks = instrs.iter().fold(stacks, f);
-    //println!("3: {:?}", stacks);
+    (stacks, instrs)
+}
 
-    let res = stacks
-        .iter()
+fn solution<F>(stacks: Vec<Vec<char>>, instrs: &Vec<(usize, usize, usize)>, f: F) -> String
+where
+    F: FnMut(Vec<Vec<char>>, &(usize, usize, usize)) -> Vec<Vec<char>>,
+{
+    //println!("2: {:?}", instrs);
+    let updated_stacks = instrs.iter().fold(stacks, f);
+    // println!("3: {:?}", stacks);
+
+    let res = updated_stacks
+        .into_iter()
         .map(|x| *x.last().unwrap() as u8)
         .collect::<Vec<_>>();
     String::from_utf8(res).unwrap()
